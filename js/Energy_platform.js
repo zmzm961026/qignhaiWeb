a43b5989324f6b6a8d65705a4253f1d8be1ee150
zmzm961026 <1046731038@qq.com>
2019-11-18 16:11:36 +0800

    添加css、js、images

---
+++ js/Energy_platform.js
@@ -0,0 +1,229 @@
+﻿onload = function () {
+    addScript('/js/js.js')
+    function addScript(url) {
+        var script = document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('src', url);
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+    var html_name = '';
+    var sort_list = '';
+    var html_logo = '';
+    var sort_box = document.getElementById('sort_box');
+    var arr = name.split(',');
+    for (var i = 0; i < arr.length; i++) {
+        html_name = '<div class="num_name">' + arr[i] + '</div>';
+        html_logo = '<div class="num_logo"><img src="' + images[i] + '"/></div>';
+        sort_list += '<div class="sort_list"><a target="_blank" href="' + addree[i] + '">' + html_name + html_logo + '</a></div>';
+    }
+    sort_box.innerHTML = sort_list;
+    //导航显示隐藏
+    $('.nav>li:eq(12)').addClass('active').siblings().removeClass('active');
+    $('.nav>li:eq(4)').addClass('active').siblings().removeClass('active');
+    $('.nav>li').hover(function () {
+        $(".nav > li").find('ul.nav-list').hide();
+        $(this).find('ul').show();
+        $(this).addClass('active').siblings().removeClass('active')
+    }, function () {
+        $(".nav > li:eq(12)").addClass('active').siblings().removeClass('active');
+        $(".nav > li:eq(4)").addClass('active').siblings().removeClass('active');
+        $(this).find('ul').hide();
+    })
+    function back() {
+        $('[class*="-two"]').css('display', 'none');
+        $('.sort_box').css('display', 'block');
+        $('.initials').css('display', 'block');
+        $('.header1').css('display', 'block');
+    }
+    $(function () {
+        var LetterTop1 = 0;
+        window.onresize = function () {
+            if ($(window).height() < 400) {
+                $('.nav').hide();
+            } else {
+                $('.nav').show();
+            }
+        }
+        $('[class*="-two"]').css('display', 'none');
+        $('#gosearch').click(function () {
+            $('[class*="-two"]').css('display', 'block');
+            $('.sort_box').css('display', 'none');
+            $('.header1').css('display', 'none');
+            $('.initials').css('display', 'none');
+            $('#search').focus();
+        });
+        $('#search').bind('input propertychange', function () {
+            $('.sort_box-two').html('');
+            //进行相关操作
+            var val = $(this).val();
+            if (val == "") {
+                return;
+            }
+            var str = "";
+            //去查找原字符串及其拼音首字母是否包含此字符，若包含就把它加进去
+            $(".sort_list").each(function () {
+                var name = $(this).find('.num_name').text();
+                if (name.toLowerCase().indexOf(val.toLowerCase()) != -1) {//包含
+                    str += "<div class='sort_list-two' id='" + $(this).attr('id') + "'> " + $(this).html() + "</div>";
+                } else {
+                    var PYarr = makePy(name);
+                    for (var i = 0; i < PYarr.length; i++) {
+                        if (PYarr[i].toLowerCase().indexOf(val.toLowerCase()) != -1) {//包含
+                            str += "<div class='sort_list-two' id='" + $(this).attr('id') + "'> " + $(this).html() + "</div>";
+                        } else {//不包含
+                        }
+                    }
+                }
+            });
+            $('.sort_box-two').html(str);
+        });
+        var defaults = {
+            items: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+            debug: false,
+            height: null,
+            arrows: true
+        };
+        $(document).ready(function () {
+            $('#list').sliderNav();
+        });
+        $.fn.sliderNav = function (options) {
+            var opts = $.extend(defaults, options);
+            var o = $.meta ? $.extend({}, opts, $$.data()) : opts;
+            $('.initials').find('ul').append('<li>A</li><li>B</li><li>C</li><li>D</li><li>E</li><li>F</li><li>G</li><li>H</li><li>I</li><li>J</li><li>K</li><li>L</li><li>M</li><li>N</li><li>O</li><li>P</li><li>Q</li><li>R</li><li>S</li><li>T</li><li>U</li><li>V</li><li>W</li><li>X</li><li>Y</li><li>Z</li>');
+            initials();
+            var slider = $(this);
+            $('.initials ul li', slider).click(function (event) {
+                var letter = '#' + $(this).text();
+                //var target = $('#'+letter);
+                var cOffset = $('.list_box', slider).offset().top;
+                var tOffset = $('.list_box ' + letter, slider).offset().top;
+                var height = $('.initials', slider).height();
+                if (o.height) height = o.height;
+                var pScroll = (tOffset - cOffset) - height / 8;
+                $('.sort_letter', slider).removeClass('selected');
+                //$(target).addClass('selected');
+                $('.list_box', slider).stop().animate({
+                    scrollTop: '+=' + pScroll + 'px'
+                });
+                if (o.debug) $('#debug span', slider).html(tOffset);
+            });
+        }
+    })
+    var Initials = document.getElementsByClassName('initials')[0];
+    var Initials_li = document.getElementsByTagName('li');
+    for (var i = 0; i < Initials_li.length; i++) {
+        Initials_li[i].height = Initials.height / 26;
+    }
+    function initials() {//公众号排序
+        var SortList = $(".sort_list");
+        var SortBox = $(".sort_box");
+        SortList.sort(asc_sort).appendTo('.sort_box');//按首字母排序
+        function asc_sort(a, b) {
+            return makePy($(b).find('.num_name').text().charAt(0))[0].toUpperCase() < makePy($(a).find('.num_name').text().charAt(0))[0].toUpperCase() ? 1 : -1;
+        }
+        var initials = [];
+        var num = 0;
+        SortList.each(function (i) {
+            var initial = makePy($(this).find('.num_name').text().charAt(0))[0].toUpperCase();
+            if (initial >= 'A' && initial <= 'Z') {
+                if (initials.indexOf(initial) === -1)
+                    initials.push(initial);
+            } else {
+                num++;
+            }
+        });
+        $.each(initials, function (index, value) {//添加首字母标签
+            SortBox.append('<div class="sort_letter" id="' + value + '">' + value + '</div>');
+        });
+        if (num != 0) { SortBox.append('<div class="sort_letter" id="default">#</div>'); }
+        for (var i = 0; i < SortList.length; i++) {//插入到对应的首字母后面
+            var letter = makePy(SortList.eq(i).find('.num_name').text().charAt(0))[0].toUpperCase();
+            switch (letter) {
+                case "A": $('#A').after(SortList.eq(i)); break;
+                case "B": $('#B').after(SortList.eq(i)); break;
+                case "C": $('#C').after(SortList.eq(i)); break;
+                case "D": $('#D').after(SortList.eq(i)); break;
+                case "E": $('#E').after(SortList.eq(i)); break;
+                case "F": $('#F').after(SortList.eq(i)); break;
+                case "G": $('#G').after(SortList.eq(i)); break;
+                case "H": $('#H').after(SortList.eq(i)); break;
+                case "I": $('#I').after(SortList.eq(i)); break;
+                case "J": $('#J').after(SortList.eq(i)); break;
+                case "K": $('#K').after(SortList.eq(i)); break;
+                case "L": $('#L').after(SortList.eq(i)); break;
+                case "M": $('#M').after(SortList.eq(i)); break;
+                case "N": $('#N').after(SortList.eq(i)); break;
+                case "O": $('#O').after(SortList.eq(i)); break;
+                case "P": $('#P').after(SortList.eq(i)); break;
+                case "Q": $('#Q').after(SortList.eq(i)); break;
+                case "R": $('#R').after(SortList.eq(i)); break;
+                case "S": $('#S').after(SortList.eq(i)); break;
+                case "T": $('#T').after(SortList.eq(i)); break;
+                case "U": $('#U').after(SortList.eq(i)); break;
+                case "V": $('#V').after(SortList.eq(i)); break;
+                case "W": $('#W').after(SortList.eq(i)); break;
+                case "X": $('#X').after(SortList.eq(i)); break;
+                case "Y": $('#Y').after(SortList.eq(i)); break;
+                case "Z": $('#Z').after(SortList.eq(i)); break;
+                default: $('#default').after(SortList.eq(i)); break;
+            }
+        }
+    }
+    var arr_html = '';
+    var arr_x = [];
+    var arr_y = [];
+    for (var i = 0; i < nes.length; i++) {
+        arr_html = nes[i].Item1.split(',')[0] + nes[i].Item1.split(',')[1];
+        arr_x.push(nes[i].Item1.split(',')[0]);
+        arr_y.push(nes[i].Item1.split(',')[1]);
+    }
+    var opts = {
+        width: 200,     // 信息窗口宽度
+        height: 100,     // 信息窗口高度
+        title: "海底捞王府井店", // 信息窗口标题
+        enableMessage: true,//设置允许信息窗发送短息
+        message: "亲耐滴，晚上一起吃个饭吧？戳下面的链接看下地址喔~"
+    }
+    var map = new BMap.Map("map");
+    var data = [];
+    map.enableScrollWheelZoom(true);
+    addMarker(points, arr_x, arr_y);
+    function addMarker(points, arr_x, arr_y) {  // 创建图标对象
+        // 创建标注对象并添加到地图
+        for (var i = 0, pointsLen = points.length; i < pointsLen; i++) {
+            var point = new BMap.Point(arr_x[i], arr_y[i]);
+            data.push({ mapx: arr_x[i], mapy: arr_y[i], name: arr[i], src: addree[i] });
+        }
+    }
+    map.centerAndZoom(new BMap.Point(116.404, 39.915), 9);
+    var markers = new Array();
+    $.each(data, function (i, item) {
+        var point = new BMap.Point(item.mapx, item.mapy);
+        var marker = new BMap.Marker(point);
+        var content = item.name;
+        var src = item.src;
+        addClickHandler(content, marker, src); //添加点击事件
+        markers.push(marker);
+    });
+    //添加聚合效果。
+    var markerClusterer = new BMapLib.MarkerClusterer(map, { markers: markers });
+    var opts = {
+        width: 250, // 信息窗口宽度
+        height: 80, // 信息窗口高度
+        title: "", // 信息窗口标题
+        enableMessage: true//设置允许信息窗发送短息
+    };
+    function addClickHandler(content, marker, src) {
+        marker.addEventListener("click", function (e) {
+            openInfo(content, e, src)
+        }
+        );
+    }
+    function openInfo(content, e, src) {
+        var p = e.target;
+        var point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
+        // 创建信息窗口对象
+        var infoWindow = new BMap.InfoWindow("大学名称：" + content + '<br/>' + "<a target='_blank' href=" + src + ">进入管理系统</a>", opts);
+        map.openInfoWindow(infoWindow, point); //开启信息窗口
+    }
+}
---
+++ js/js.js
@@ -0,0 +1,237 @@
+﻿function tab(id) {
+    return document.getElementById(id);
+}
+if (localStorage.getItem('user_name') && localStorage.getItem('user_pass_word')) {
+    tab('login_btn').innerHTML = '账号：' + localStorage.getItem('user_name');
+} 
+tab('login_btn').onclick = function () {
+    login_btn();
+};
+//登录设置
+var oClose = document.getElementsByClassName('close');
+var oTeach = document.getElementsByName('teach');
+var aTeach_visited = tab('zhuce').getElementsByClassName('teach_visited');
+function div_hide() {
+    //清空提示框的值
+    var allB = tab('login').getElementsByTagName('b');
+    for (var i = 0; i < allB.length; i++) {
+        allB[i].innerHTML = '';
+    }
+    //清空input值
+    $('input').val('');
+    //注册隐藏
+    tab('zhuce').style.display = 'none';
+    //重置密码隐藏
+    tab('pass_change').style.display = 'none';
+    //管理员登录隐藏
+    tab('admin').style.display = "none";
+    //登录隐藏
+    tab('log').style.display = 'none';
+    tab('login').style.display = 'block';
+}
+function login_btn() {
+    div_hide();
+    tab('log').style.display = 'block';
+    //注册身份切换
+    tab('registe').onclick = function () {
+        registe();
+    }
+    tab('forget_pass').onclick = function () {
+        div_hide();
+        tab('pass_change').style.display = 'block';
+    }
+    tab('login_but').onclick = function () {
+        loginIn();
+    }
+}
+tab('pass_btn').onclick = function () {
+    registe();
+}
+//注册信息切换
+function registe() {
+    div_hide();
+    tab('zhuce').style.display = 'block';
+    //老师注册
+    //tab('zhuce').style.height = 598 + 'px';
+    //tab('zhuce').style.marginTop = -598 / 2 + 'px';
+    tab('zhuce').style.height = 428 + 'px';
+    tab('zhuce').style.marginTop = -428 / 2 + 'px';
+    aTeach_visited[0].style.display = 'block';
+    oTeach[0].checked = true;
+}
+createCode();
+var code ; //在全局 定义验证码
+function createCode(){ 
+    code = new Array();
+    var codeLength = 4;//验证码的长度
+    var checkCode = document.getElementById("checkCode");
+    checkCode.innerHTML = "";
+
+    var selectChar = new Array(2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z');
+
+    for(var i=0;i<codeLength;i++) {
+    var charIndex = Math.floor(Math.random()*32);
+        code +=selectChar[charIndex];
+    }
+    if(code.length != codeLength){
+        createCode();
+    }
+    checkCode.innerHTML = code;
+}
+var myreg = /^1[3456789]\d{9}$/;
+var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+var reg1 = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/;
+var zcInput = tab('visited').getElementsByTagName('input');
+var one = tab('visited').getElementsByTagName('b');
+//注册点击事件
+var code ; 
+$('#but').click(function () {
+    var phone = $('.sjh').val();
+    var pass = document.getElementsByName('password')[0].value;
+    var pass1 = $('#ipu').val();
+    var pass2 = $('#ipu1').val();
+    var getcode = document.getElementById("input1").value.toUpperCase();
+    for (var i = 0; i < zcInput.length-2; i++) {
+        if (zcInput[i].value == '') {
+            one[i].innerHTML = '<span>*</span>必填';
+            one[i].style.lineHeight = 34 + 'px';
+            one[3].innerHTML = '';
+        } else {
+            one[i].innerHTML = '';
+        }
+    }
+    var weekArray = []    
+    console.log(getcode+":"+code)
+    if (pass1 === pass2 && pass1.length >= 6 && pass1.length <= 16 && getcode == code) {
+        $.ajax({
+            url: 'http://192.168.1.167:325/api/Cour/Register',
+            type: 'get',
+            dataType: 'json',
+            data: {Pass: pass1, User:phone},
+            success: function (data) {
+                console.log(data)
+                if (data == false) {
+                    alert('该账户已被注册');
+                    tab('login').style.display = 'none';
+                }
+                if (data == true) {
+                    alert('注册成功，等待审核');
+                    weekArray.push(phone)
+                    console.log(weekArray)
+                    tab('login').style.display = 'none';
+                    localStorage.setItem('s_name', JSON.stringify(weekArray));
+                }
+            }
+        })
+        
+    } else if ($('.sjh').val() == '') {
+        one[0].innerHTML = '<span>*</span>必填';
+        return;
+    } else  if (pass1 == '') {
+        one[1].innerHTML = '<span>*</span>必填';
+        return;
+    } else if (pass2 == '') {
+        one[2].innerHTML = '<span>*</span>必填';
+        return;
+    } else if (!reg1.test(pass1) || !reg1.test(pass2) || pass1.length <= 6 && pass1.length >= 17 || pass2.length <= 6 && pass2.length >= 17) {
+        one[1].innerHTML = '<span>*</span>请输入6-16位字母、数字组成的密码';
+        one[2].innerHTML = '<span>*</span>请输入6-16位字母、数字组成的密码';
+        one[1].style.lineHeight = 20 + 'px';
+        one[2].style.lineHeight = 20 + 'px';
+        return;
+    } else if (pass1 !== pass2) {
+        one[2].style.lineHeight = 34 + 'px';
+        one[2].innerHTML = '<span>*</span>两次密码不一致';
+        return;
+    } else if (getcode == '') {
+        one[4].innerHTML = '<span>*</span>验证码不能为空';
+        return;
+    } else if (getcode != code)  {
+        one[4].innerHTML = '<span>*</span>验证码输入错误！';
+        createCode();
+        return false;
+    }
+})
+//注册页面返回登录
+tab('zhuce_dl').onclick = function () {
+    login_btn();
+}
+//管理员权限登录
+$('#admin_but').click(function () {
+    if (admin[0] == $('.admin').val() && admin_pass[0] == $('.admin_pass').val()) {
+        alert('登录成功');
+        setCookie('Admin', $('.admin').val());
+        setCookie('Admin_pass', $('.admin_pass').val());
+        function setCookie(k, v) {
+            var date = new Date((new Date()).getTime() + 2 * 60 * 60000);
+            document.cookie = k + '=' + escape(v) + ';expires=' + date + "; path=/;";
+        }
+        window.location.reload();
+    } else if ($('.admin').val() == '') {
+        alert('账号不能为空');
+        return false;
+    } else {
+        alert('账号或密码输入错误');
+        return false;
+    }
+})
+//关闭按钮点击事件
+for (var i = 0; i < oClose.length; i++) {
+    oClose[i].index = i;
+    oClose[i].onclick = function () {
+        var aInput = document.getElementsByTagName('input');
+        var abb = tab('login').getElementsByTagName('b');
+        for (var i = 0; i < aInput.length; i++) {
+            aInput[i].value = '';
+        }
+        for (var i = 0; i < abb.length; i++) {
+            abb[i].innerHTML = '';
+        }
+        tab('login').style.display = 'none';
+    }
+}
+//登录按钮点击事件
+function loginIn() {
+    if ($('.username').val()!=''&& $('.password_log').val()!='') {
+        $.ajax({
+            url: 'http://192.168.1.167:325/api/Cour/Login',
+            type: 'get',
+            dataType: 'json',
+            data: { Pass: $('.password_log').val(), User: $('.username').val() },
+            success: function (data) {
+                console.log(data)
+                if (data == 'false') {
+                    alert('用户名不存在或密码错误');
+                    return false;
+                } else {
+                    alert('登录成功');
+                    localStorage.setItem('user_name', $('.username').val());
+                    localStorage.setItem('user_pass_word', $('.password_log').val());
+                    localStorage.setItem('state', data[0]);
+                    localStorage.setItem('state_id', data[1]);
+                    // function setCookie(k, v) {
+                    //     var date = new Date((new Date()).getTime() + 2 * 60 * 60000);
+                    //     document.cookie = k + '=' + escape(v) + ';expires=' + date + "; path=/;";
+                    // }
+                    tab('login').style.display = 'none';
+                    tab('login_btn').innerHTML = '账号：' + localStorage.getItem('user_name');
+                }
+            }
+        })
+    } else if ($('.username').val() == '') {
+        alert('用户名不能为空');
+        return false;
+    }  else if ($('.password_log').val() == '') {
+        alert('密码不能为空');
+        return false;
+    } 
+}
+function getCookie(name) {
+    var arr = document.cookie.split('; ');
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].split('=')[0] == name) {
+            return arr[i].split('=')[1].toString();
+        }
+    }
+    return ''
+}
